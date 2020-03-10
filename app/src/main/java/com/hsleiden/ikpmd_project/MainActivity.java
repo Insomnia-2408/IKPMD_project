d11ed4b3a0b2494931908e4b4fb25845e13d0092
Valerie <valerietimmerman2000@gmail.com>
2020-03-10 14:44:59 +0100

    Test

--- app/src/main/java/com/hsleiden/ikpmd_project/MainActivity.java
+++ app/src/main/java/com/hsleiden/ikpmd_project/MainActivity.java
@@ -3,6 +3,7 @@ package com.hsleiden.ikpmd_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        makeToast();
+    }
+
+    private void makeToast() {
+        Toast.makeText(this, "Hallo!", Toast.LENGTH_SHORT).show();
     }
 }
